c13564e716e939968deed46caea3cf7f5e8424d4
Kin Lane <info@apievangelist.com>
2016-01-11 21:38:42 -0800

    update upload

--- js/upload.js
+++ js/upload.js
@@ -1,13 +1,13 @@
-function readfile(f) {
-    var reader = new FileReader(); 
-    reader.readAsText(f); 
-    reader.onload = function() { 
-        var text = reader.result; 
+function uploadCSV(f) {
+    var reader = new FileReader();
+    reader.readAsText(f);
+    reader.onload = function() {
+        var text = reader.result;
         document.getElementById('uploadfilename').value = f.name;
-        var out = document.getElementById("dataInput");  
-        out.innerHTML = "";                          
+        var out = document.getElementById("dataInput");
+        out.innerHTML = "";
         out.appendChild(document.createTextNode(text));
-        
+
         oAuth_Token = document.getElementById('oAuth_Token').value;
         //alert(oAuth_Token);
         if(oAuth_Token!='')
@@ -17,6 +17,29 @@ function readfile(f) {
 
     }
     reader.onerror = function(e) {
-        console.log("Error", e);  
+        console.log("Error", e);
+    };
+}
+
+function uploadJSON(f) {
+    var reader = new FileReader();
+    reader.readAsText(f);
+    reader.onload = function() {
+        var text = reader.result;
+        document.getElementById('uploadfilename').value = f.name;
+        var out = document.getElementById("dataInput");
+        out.innerHTML = "";
+        out.appendChild(document.createTextNode(text));
+
+        oAuth_Token = document.getElementById('oAuth_Token').value;
+        //alert(oAuth_Token);
+        if(oAuth_Token!='')
+        	{
+        	document.getElementById("save").style.display = '';
+        	}
+        jsonOutput();
+    }
+    reader.onerror = function(e) {
+        console.log("Error", e);
     };
 }
